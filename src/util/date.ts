468faffeaddf224474c2c0dd76cc044555ecfa5e
MichaelSun <zhensherlock@126.com>
2023-05-25 17:45:31 +0800

    feat: add date utils

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,5 +1,20 @@
 import ms, { StringValue } from 'ms';
+import * as dayjs from 'dayjs';
+import { isNil, isDate } from 'lodash';
 
 export const formatToMS = (value: StringValue): number => {
   return ms(value);
+};
+
+export const dateTransformer = {
+  from: (value: Date | number | string) => {
+    if (isNil(value)) {
+      return null;
+    }
+
+    return dayjs(isDate(value) ? (<Date>value).getTime() : value).format(
+      'YYYY-MM-DD HH:mm:ss'
+    );
+  },
+  to: () => new Date(),
 };
