b69b307d5845d75aab2d5f377f70c5e867cf0ee7
MichaelSun <zhensherlock@126.com>
2023-05-27 17:04:28 +0800

    feat: Optimized base.service.ts

--- src/service/base.service.ts
+++ src/service/base.service.ts
@@ -1,5 +1,7 @@
 import { DeleteResult, UpdateResult, Repository } from 'typeorm';
 import { FindManyOptions } from 'typeorm/find-options/FindManyOptions';
+import { PickKeysByType } from 'typeorm/common/PickKeysByType';
+import { FindOptionsWhere } from 'typeorm/find-options/FindOptionsWhere';
 
 export class BaseService<T> {
   entityModel: Repository<T | any>;
@@ -16,8 +18,31 @@ export class BaseService<T> {
     return await this.entityModel.count(options);
   }
 
-  async findAll(): Promise<T[]> {
-    return this.entityModel.find();
+  async getSum(
+    columnName: PickKeysByType<T, number>,
+    where?: FindOptionsWhere<T>
+  ): Promise<number> {
+    return await this.entityModel.sum(columnName, where);
+  }
+
+  async getList(options?: FindManyOptions<T>): Promise<T[]> {
+    return await this.entityModel.find(options);
+  }
+
+  async getListAndCount(options?: FindManyOptions<T>): Promise<[T[], number]> {
+    return await this.entityModel.findAndCount(options);
+  }
+
+  async getPaginatedList(
+    currentPage = 1,
+    pageSize = 20,
+    options?: FindManyOptions<T>
+  ): Promise<[T[], number]> {
+    return await this.entityModel.findAndCount({
+      skip: (currentPage - 1) * pageSize,
+      take: pageSize,
+      ...options,
+    });
   }
 
   async saveObject(entity: T): Promise<T> {
@@ -31,7 +56,7 @@ export class BaseService<T> {
   async createObject(entity: T): Promise<T> {
     const mdl = this.entityModel.create();
     Object.assign(mdl, entity);
-    return this.entityModel.save(mdl);
+    return await this.entityModel.save(mdl);
   }
 
   async updateObject(id: string, entity: T): Promise<T> {
--- src/service/tenant.service.ts
+++ src/service/tenant.service.ts
@@ -2,10 +2,6 @@ import { Provide } from '@midwayjs/core';
 import { InjectEntityModel } from '@midwayjs/typeorm';
 import { Tenant } from '../entity/tenant.entity';
 import { Repository } from 'typeorm';
-import type {
-  // ITenantFindOneByIdOptions,
-  ISaveTenantOptions,
-} from '../interface';
 import { BaseService } from './base.service';
 
 @Provide()
@@ -15,20 +11,5 @@ export class TenantService extends BaseService<Tenant> {
 
   constructor() {
     super();
-  }
-
-  async save(options: ISaveTenantOptions) {
-    const tenant = new Tenant();
-    tenant.name = options.name;
-    tenant.description = options.description;
-    tenant.address = options.address;
-    tenant.country = options.country;
-    tenant.province = options.province;
-    tenant.city = options.city;
-    tenant.latitude = options.latitude;
-    tenant.longitude = options.longitude;
-    tenant.enabled = options.enabled;
-    const tenantResult = await this.entityModel.save(tenant);
-    console.log('tenant id = ', tenantResult.id);
   }
 }
