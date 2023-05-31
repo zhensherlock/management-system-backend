db9290e0b9990556b587c445a173a702a0659e6e
MichaelSun <zhensherlock@126.com>
2023-05-31 13:14:05 +0800

    feat: add tenant validate

--- src/dto/areas/admin/tenant.dto.ts
+++ src/dto/areas/admin/tenant.dto.ts
@@ -1,37 +1,171 @@
 import { Rule, RuleType, OmitDto } from '@midwayjs/validate';
+import { handleError, handleErrors } from '../../../error';
 
 export class TenantDTO {
   @Rule(RuleType.string().max(36).required())
   id: string;
 
-  @Rule(RuleType.string().max(100).required())
+  @Rule(
+    RuleType.string()
+      .max(100)
+      .required()
+      .error(
+        handleErrors({
+          'string.empty': {
+            message: 'i18n:name.required.message',
+            options: { group: 'tenant' },
+          },
+          'any.required': {
+            message: 'i18n:name.required.message',
+            options: { group: 'tenant' },
+          },
+          'string.max': {
+            message: 'i18n:name.length.message',
+            options: { group: 'tenant' },
+          },
+          'string.base': {
+            message: 'i18n:name.base.message',
+            options: { group: 'tenant' },
+          },
+        })
+      )
+  )
   name: string;
 
-  @Rule(RuleType.string().max(150))
+  @Rule(
+    RuleType.string()
+      .max(150)
+      .error(
+        handleErrors({
+          'string.max': {
+            message: 'i18n:description.length.message',
+            options: { group: 'tenant' },
+          },
+          'string.base': {
+            message: 'i18n:description.base.message',
+            options: { group: 'tenant' },
+          },
+        })
+      )
+  )
   description: string;
 
-  @Rule(RuleType.string().max(150))
+  @Rule(
+    RuleType.string()
+      .max(150)
+      .error(
+        handleErrors({
+          'string.max': {
+            message: 'i18n:address.length.message',
+            options: { group: 'tenant' },
+          },
+          'string.base': {
+            message: 'i18n:address.base.message',
+            options: { group: 'tenant' },
+          },
+        })
+      )
+  )
   address: string;
 
-  @Rule(RuleType.string().max(150))
+  @Rule(
+    RuleType.string()
+      .max(150)
+      .error(
+        handleErrors({
+          'string.max': {
+            message: 'i18n:country.length.message',
+            options: { group: 'tenant' },
+          },
+          'string.base': {
+            message: 'i18n:country.base.message',
+            options: { group: 'tenant' },
+          },
+        })
+      )
+  )
   country: string;
 
-  @Rule(RuleType.string().max(150))
+  @Rule(
+    RuleType.string()
+      .max(150)
+      .error(
+        handleErrors({
+          'string.max': {
+            message: 'i18n:province.length.message',
+            options: { group: 'tenant' },
+          },
+          'string.base': {
+            message: 'i18n:province.base.message',
+            options: { group: 'tenant' },
+          },
+        })
+      )
+  )
   province: string;
 
-  @Rule(RuleType.string().max(150))
+  @Rule(
+    RuleType.string()
+      .max(150)
+      .error(
+        handleErrors({
+          'string.max': {
+            message: 'i18n:city.length.message',
+            options: { group: 'tenant' },
+          },
+          'string.base': {
+            message: 'i18n:city.base.message',
+            options: { group: 'tenant' },
+          },
+        })
+      )
+  )
   city: string;
 
-  @Rule(RuleType.number().min(-90).max(90))
+  @Rule(
+    RuleType.number()
+      .min(-90)
+      .max(90)
+      .error(
+        handleError({
+          message: 'i18n:latitude.base.message',
+          options: { group: 'tenant' },
+        })
+      )
+  )
   latitude: number;
 
-  @Rule(RuleType.number().min(-180).max(180))
+  @Rule(
+    RuleType.number()
+      .min(-180)
+      .max(180)
+      .error(
+        handleError({
+          message: 'i18n:longitude.base.message',
+          options: { group: 'tenant' },
+        })
+      )
+  )
   longitude: number;
 
-  @Rule(RuleType.boolean())
+  @Rule(
+    RuleType.boolean().error(
+      handleError({
+        message: 'i18n:enabled.base.message',
+        options: { group: 'tenant' },
+      })
+    )
+  )
   enabled: boolean;
 
-  @Rule(RuleType.object())
+  @Rule(
+    RuleType.object().error(
+      handleError({
+        message: 'i18n:options.base.message',
+        options: { group: 'tenant' },
+      })
+    )
+  )
   options: object;
 }
 
--- src/filter/validate.filter.ts
+++ src/filter/validate.filter.ts
@@ -1,13 +1,26 @@
 import { Catch } from '@midwayjs/core';
 import { MidwayValidationError } from '@midwayjs/validate';
 import { Context } from '@midwayjs/koa';
+import { MidwayI18nService } from '@midwayjs/i18n';
+import { isObject } from 'lodash';
+import { tryParseJSON } from '../util';
 
 @Catch(MidwayValidationError)
 export class ValidateErrorFilter {
   async catch(err: MidwayValidationError, ctx: Context) {
+    let message = err.message;
+    const i18nService = await ctx.requestContext.getAsync(MidwayI18nService);
+    const obj = tryParseJSON(err.message);
+    if (isObject(obj)) {
+      message = obj.message;
+      if (obj.message.startsWith('i18n:')) {
+        const i18nKey = obj.message.match(/^i18n:(.*)$/)[1];
+        message = i18nService.translate(i18nKey, obj.options);
+      }
+    }
     return {
       status: 422,
-      message: '校验参数错误,' + err.message,
+      message,
     };
   }
 }
