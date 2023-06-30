06f2660ff1bcbbf0d564d2edb0690dc602afee36
MichaelSun <zhensherlock@126.com>
2023-06-30 11:01:37 +0800

    feat: add module controller

---
+++ src/controller/areas/user/module.controller.ts
@@ -0,0 +1,40 @@
+import { Inject, Controller, Get } from '@midwayjs/core';
+import { Context } from '@midwayjs/koa';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@midwayjs/swagger';
+import { MidwayI18nService } from '@midwayjs/i18n';
+import { BaseUserController } from './base/base.user.controller';
+import { Role } from '../../../decorator/role.decorator';
+import { ModuleService } from '../../../service/module.service';
+
+@ApiBearerAuth()
+@ApiTags(['user'])
+@Controller('/api/user/module')
+export class ModuleController extends BaseUserController {
+  @Inject()
+  ctx: Context;
+
+  @Inject()
+  moduleService: ModuleService;
+
+  @Inject()
+  i18nService: MidwayI18nService;
+
+  @Role(['school', 'security'])
+  @Get('/list', { summary: '用户-查询可用模块列表' })
+  @ApiQuery({})
+  async getModuleList() {
+    const user = this.ctx.currentUser;
+    const roleIds = user.userRoleMappings.map(item => item.roleId);
+    const moduleRoleMappings = (<string[]>roleIds).map(id => ({
+      roleId: id,
+    }));
+    const modules = await this.moduleService.getList({
+      where: {
+        moduleRoleMappings,
+      },
+    });
+    return {
+      modules,
+    };
+  }
+}
