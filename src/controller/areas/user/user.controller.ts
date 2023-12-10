89fbd3f6d14e593883e6dbf935704038f0577796
MichaelSun <zhensherlock@126.com>
2023-12-10 20:54:45 +0800

    feat: add user controller

---
+++ src/controller/areas/user/user.controller.ts
@@ -0,0 +1,142 @@
+import {
+  Inject,
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Del,
+  File,
+} from '@midwayjs/core';
+import { Context } from '@midwayjs/koa';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+  BodyContentType,
+} from '@midwayjs/swagger';
+import { MidwayI18nService } from '@midwayjs/i18n';
+import { BaseUserController } from './base/base.user.controller';
+import { Role } from '../../../decorator/role.decorator';
+import { UserService } from '../../../service/user.service';
+import {
+  CreateUserDTO,
+  GetUserListDTO,
+  UpdateUserDTO,
+} from '../../../dto/areas/user/user.dto';
+import { isEmpty, omit } from 'lodash';
+import { Like } from 'typeorm';
+import { CommonError } from '../../../error';
+import { UserEntity } from '../../../entity/user.entity';
+
+@ApiBearerAuth()
+@ApiTags(['user'])
+@Controller('/api/user/user')
+export class UserController extends BaseUserController {
+  @Inject()
+  ctx: Context;
+
+  @Inject()
+  userService: UserService;
+
+  @Inject()
+  i18nService: MidwayI18nService;
+
+  @Role(['school', 'security', 'education'])
+  @Get('/list', { summary: '用户-查询用户列表' })
+  @ApiQuery({})
+  async getUserList(@Query() query: GetUserListDTO) {
+    const [list, count, currentPage, pageSize] =
+      await this.userService.getPaginatedList(
+        query.currentPage,
+        query.pageSize,
+        {
+          where: {
+            ...(isEmpty(query.keyword)
+              ? {}
+              : { name: Like(`%${query.keyword}%`) }),
+          },
+          order: {
+            updatedDate: 'DESC',
+          },
+        }
+      );
+    return {
+      list,
+      count,
+      currentPage,
+      pageSize,
+    };
+  }
+
+  @Role(['education'])
+  @Post('/import', { summary: '用户-导入用户列表' })
+  @ApiBody({
+    description: '用户数据文件',
+    contentType: BodyContentType.Multipart,
+  })
+  async import(@File() file) {
+    // await this.userService.importList(file.data);
+    return this.i18nService.translate('import.success', { group: 'global' });
+  }
+
+  @Role(['education'])
+  @Post('/create', { summary: '用户-新建用户' })
+  @ApiBody({ description: '用户信息' })
+  async createUser(@Body() dto: CreateUserDTO) {
+    const tenantId = this.ctx.currentUser.tenantId;
+    if (await this.userService.checkNameExisted(dto.name, tenantId)) {
+      throw new CommonError('name.exist.message', { group: 'user' });
+    }
+    const user = <UserEntity>dto;
+    user.enabled = true;
+    const mdl = await this.userService.createObject(<UserEntity>dto);
+    return omit(mdl, ['deletedDate']);
+  }
+
+  @Role(['education'])
+  @Put('/:id', { summary: '用户-修改用户' })
+  @ApiParam({ name: 'id', description: '编号' })
+  @ApiBody({ description: '用户信息' })
+  async updateUser(@Param('id') id: string, @Body() dto: UpdateUserDTO) {
+    const mdl = await this.userService.getOneObject({
+      where: {
+        id,
+      },
+    });
+    if (!mdl) {
+      throw new CommonError('not.exist', { group: 'global' });
+    }
+    if (await this.userService.checkNameExisted(dto.name, id)) {
+      throw new CommonError('name.exist.message', { group: 'user' });
+    }
+
+    Object.assign(mdl, dto);
+
+    return omit(await this.userService.updateObject(mdl), ['deletedDate']);
+  }
+
+  @Role(['education'])
+  @Del('/:id', { summary: '用户-删除用户' })
+  @ApiParam({ name: 'id', description: '编号' })
+  async deleteUser(@Param('id') id: string) {
+    if (
+      !(await this.userService.existObject({
+        where: {
+          id,
+        },
+      }))
+    ) {
+      throw new CommonError('not.exist', { group: 'global' });
+    }
+    const result = await this.userService.deleteObject(id);
+    if (!result.affected) {
+      throw new CommonError('delete.failure', { group: 'global' });
+    }
+    return this.i18nService.translate('delete.success', { group: 'global' });
+  }
+}
---
+++ src/dto/areas/user/user.dto.ts
@@ -0,0 +1,143 @@
+import { Rule, RuleType } from '@midwayjs/validate';
+import { handleParameterError, handleParameterErrors } from '../../../error';
+import { GetListBaseDTO } from '../../base.dto';
+import { ApiProperty } from '@midwayjs/swagger';
+import { UserType } from '../../../constant';
+
+export class UserDTO {
+  @ApiProperty({ example: 'user1', description: '用户登录名' })
+  @Rule(
+    RuleType.string()
+      .max(100)
+      .required()
+      .trim(true)
+      .error(
+        handleParameterErrors({
+          'string.empty': {
+            message: 'name.required.message',
+            options: { group: 'user' },
+          },
+          'any.required': {
+            message: 'name.required.message',
+            options: { group: 'user' },
+          },
+          'string.max': {
+            message: 'name.length.message',
+            options: { group: 'user' },
+          },
+          '*': {
+            message: 'name.base.message',
+            options: { group: 'user' },
+          },
+        })
+      )
+  )
+  name: string;
+
+  @ApiProperty({ example: 'user1@hy.com', description: '用户邮箱' })
+  @Rule(
+    RuleType.string()
+      .email()
+      .max(100)
+      .empty('')
+      .trim(true)
+      .error(
+        handleParameterErrors({
+          'string.max': {
+            message: 'email.length.message',
+            options: { group: 'user' },
+          },
+          'string.email': {
+            message: 'email.base.message',
+            options: { group: 'user' },
+          },
+          '*': {
+            message: 'email.base.message',
+            options: { group: 'user' },
+          },
+        })
+      )
+  )
+  email: string;
+
+  @ApiProperty({ example: '123456789999', description: '用户电话' })
+  @Rule(
+    RuleType.string()
+      .empty('')
+      .trim(true)
+      .pattern(
+        /((\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$)/
+      )
+      .error(
+        handleParameterError({
+          message: 'tel.base.message',
+          options: { group: 'user' },
+        })
+      )
+  )
+  tel: string;
+
+  @ApiProperty({ example: '1', description: '用户类型' })
+  @Rule(
+    RuleType.string()
+      .required()
+      .max(1)
+      .trim(true)
+      .valid(...Object.values(UserType))
+      .error(
+        handleParameterError({
+          message: 'type.base.message',
+          options: { group: 'user' },
+        })
+      )
+  )
+  type: string;
+
+  @ApiProperty({ example: 'Michael', description: '用户真实姓名' })
+  @Rule(
+    RuleType.string()
+      .max(100)
+      .empty('')
+      .trim(true)
+      .error(
+        handleParameterErrors({
+          'string.max': {
+            message: 'real_name.length.message',
+            options: { group: 'user' },
+          },
+          '*': {
+            message: 'real_name.base.message',
+            options: { group: 'user' },
+          },
+        })
+      )
+  )
+  realName: string;
+
+  @ApiProperty({ example: '新的用户', description: '用户介绍' })
+  @Rule(
+    RuleType.string()
+      .max(150)
+      .empty('')
+      .trim(true)
+      .error(
+        handleParameterErrors({
+          'string.max': {
+            message: 'description.length.message',
+            options: { group: 'user' },
+          },
+          '*': {
+            message: 'description.base.message',
+            options: { group: 'user' },
+          },
+        })
+      )
+  )
+  description: string;
+}
+
+export class CreateUserDTO extends UserDTO {}
+
+export class UpdateUserDTO extends UserDTO {}
+
+export class GetUserListDTO extends GetListBaseDTO {}
